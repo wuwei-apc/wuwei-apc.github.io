49f3af3dc7aee007eaf43d31c5168ddd6d4c5b40
wuwei-apc <wuwei-apc@users.noreply.github.com>
2023-08-23 03:54:38 +0000

    deploy: a33970502671783308c4dcab4a0f04f85aa8e95e

--- js/layouts/toc.js
+++ js/layouts/toc.js
@@ -1,18 +1,18 @@
 /* global function */
+
 function initTOC() {
-  Global.utils.navItems = document.querySelectorAll('.post-toc-wrap .post-toc li');
+  Global.utils.navItems = document.querySelectorAll(
+    ".post-toc-wrap .post-toc li",
+  );
 
   if (Global.utils.navItems.length > 0) {
-
     Global.utils = {
-
       ...Global.utils,
-      
 
       updateActiveTOCLink() {
         if (!Array.isArray(Global.utils.sections)) return;
-        let index = Global.utils.sections.findIndex(element => {
-          return element && element.getBoundingClientRect().top - 200 > 0;
+        let index = Global.utils.sections.findIndex((element) => {
+          return element && element.getBoundingClientRect().top - 100 > 0;
         });
         if (index === -1) {
           index = Global.utils.sections.length - 1;
@@ -23,95 +23,92 @@ function initTOC() {
       },
 
       registerTOCScroll() {
-        Global.utils.sections = [...document.querySelectorAll('.post-toc li a.nav-link')].map(element => {
-          const target = document.getElementById(decodeURI(element.getAttribute('href')).replace('#', ''));
-          element.addEventListener('click', event => {
-            event.preventDefault();
-            const offset = target.getBoundingClientRect().top + window.scrollY;
-            window.anime({
-              targets: document.scrollingElement,
-              duration: 500,
-              easing: 'linear',
-              scrollTop: offset - 10,
-              complete: function () {
-                setTimeout(() => {
-                  Global.utils.pageTop_dom.classList.add('hide');
-                }, 100)
-              }
-            });
-          });
+        Global.utils.sections = [
+          ...document.querySelectorAll(".post-toc li a.nav-link"),
+        ].map((element) => {
+          const target = document.getElementById(
+            decodeURI(element.getAttribute("href")).replace("#", ""),
+          );
           return target;
         });
       },
 
-
       activateTOCLink(index) {
-        const target = document.querySelectorAll('.post-toc li a.nav-link')[index];
-      
-        if (!target || target.classList.contains('active-current')) {
+        const target = document.querySelectorAll(".post-toc li a.nav-link")[
+          index
+        ];
+
+        if (!target || target.classList.contains("active-current")) {
           return;
         }
-      
-        document.querySelectorAll('.post-toc .active').forEach(element => {
-          element.classList.remove('active', 'active-current');
+
+        document.querySelectorAll(".post-toc .active").forEach((element) => {
+          element.classList.remove("active", "active-current");
         });
-        target.classList.add('active', 'active-current');
-      
+        target.classList.add("active", "active-current");
         // Scroll to the active TOC item
-        const tocElement = document.querySelector('.toc-content-container');
+        const tocElement = document.querySelector(".toc-content-container");
         const tocTop = tocElement.getBoundingClientRect().top;
-        const scrollTopOffset = tocElement.offsetHeight > window.innerHeight ? (tocElement.offsetHeight - window.innerHeight) / 2 : 0;
+        const scrollTopOffset =
+          tocElement.offsetHeight > window.innerHeight
+            ? (tocElement.offsetHeight - window.innerHeight) / 2
+            : 0;
         const targetTop = target.getBoundingClientRect().top - tocTop;
-        const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        const distanceToCenter = targetTop - (viewportHeight / 2) + (target.offsetHeight / 2) - scrollTopOffset;
+        const viewportHeight = Math.max(
+          document.documentElement.clientHeight,
+          window.innerHeight || 0,
+        );
+        const distanceToCenter =
+          targetTop -
+          viewportHeight / 2 +
+          target.offsetHeight / 2 -
+          scrollTopOffset;
         const scrollTop = tocElement.scrollTop + distanceToCenter;
-        
-        window.anime({
-          targets: tocElement,
-          duration: 300,
-          easing: 'easeOutQuad',
-          scrollTop: scrollTop
+
+        tocElement.scrollTo({
+          top: scrollTop,
+          behavior: "smooth", // Smooth scroll
         });
       },
-      
-      
 
       showTOCAside() {
-
         const openHandle = () => {
           const styleStatus = Global.getStyleStatus();
-          const key = 'isOpenPageAside';
+          const key = "isOpenPageAside";
           if (styleStatus && styleStatus.hasOwnProperty(key)) {
             Global.utils.TocToggle.pageAsideHandleOfTOC(styleStatus[key]);
           } else {
             Global.utils.TocToggle.pageAsideHandleOfTOC(true);
           }
-        }
+        };
 
-        const initOpenKey = 'init_open';
+        const initOpenKey = "init_open";
 
         if (Global.theme_config.articles.toc.hasOwnProperty(initOpenKey)) {
-          Global.theme_config.articles.toc[initOpenKey] ? openHandle() : Global.utils.TocToggle.pageAsideHandleOfTOC(false);
-
+          Global.theme_config.articles.toc[initOpenKey]
+            ? openHandle()
+            : Global.utils.TocToggle.pageAsideHandleOfTOC(false);
         } else {
           openHandle();
         }
-
-      }
-    }
+      },
+    };
 
     Global.utils.showTOCAside();
     Global.utils.registerTOCScroll();
-
   } else {
-    document.querySelectorAll('.toc-content-container, .toc-marker').forEach((elem) => {
-      elem.remove();
-    });
+    document
+      .querySelectorAll(".toc-content-container, .toc-marker")
+      .forEach((elem) => {
+        elem.remove();
+      });
   }
 }
 
-if (Global.theme_config.global.pjax === true && Global.utils) {
-  initTOC();
-} else {
-  window.addEventListener('DOMContentLoaded', initTOC);
-}
+try {
+  swup.hooks.on("page:view", () => {
+    initTOC();
+  });
+} catch (e) {}
+
+document.addEventListener("DOMContentLoaded", initTOC);
